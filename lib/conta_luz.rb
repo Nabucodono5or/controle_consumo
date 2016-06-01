27d55aae07a93558e33e46e5fec8627487804b83
jefferson abreu <nabucodonosor310@gmail.com>
2016-06-01 17:19:22 -0300

    refatoramento para attr_accessor passou

--- lib/conta_luz.rb
+++ lib/conta_luz.rb
@@ -5,40 +5,21 @@ require 'data_nominal'
 class ContaLuz
  # codigo para implementar
     attr_accessor :formato_europeu
+    attr_accessor :qtd_kw_gasto
+    attr_accessor :valor_pagar
+    attr_accessor :numero_leitura
+    attr_reader :mes
+    attr_accessor :ano
 
     def initialize
         @formato_europeu = '%d/%m/%Y'
-    end
-
-    def qtd_kw_gasto
-       @qtd_kw_gasto
-    end
-
-    def qtd_kw_gasto=(qtd_gasto)
-       @qtd_kw_gasto = qtd_gasto
-    end
-
-    def valor_pagar=(valor)
-        @valor_pagar = valor
-    end
-    
-    def valor_pagar
-        @valor_pagar
-    end
-
-    def numero_leitura=(numero)
-        @numero_leitura = numero
-    end
-
-    def numero_leitura
-        @numero_leitura
     end
 
     def vencimento=(string)        
         @vencimento = Date.strptime(string, formato_europeu)
     end
 
-    def vencimento
+    def vencimento#adcionar um metodo attr_reader para vencimento e trocar metodo de impressão
         @vencimento.day.to_s << "/" << @vencimento.month.to_s << "/" << @vencimento.year.to_s
     end
 
@@ -46,26 +27,14 @@ class ContaLuz
         @emissao = Date.strptime(string, formato_europeu)        
     end
 
-    def emissao
+    def emissao#mesma coisa que vencimento
         @emissao.day.to_s << "/" << @emissao.month.to_s << "/" << @emissao.year.to_s
     end
 
     def mes=(mes)
         @mes = mes.to_i
     end
-
-    def mes
-        @mes
-    end
     
-    def ano=(ano)
-        @ano = ano
-    end
-    
-    def ano
-        @ano
-    end
-
     def nominal
         @nominal =  DataNominal.new mes if mes
         @nominal.nominal
