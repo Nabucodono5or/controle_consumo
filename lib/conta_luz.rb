ce2ffe83cc7c52c8528f16ee1205827d90d23a8c
jefferson abreu <nabucodonosor310@gmail.com>
2016-06-01 19:09:05 -0300

    organizando comentários

--- lib/conta_luz.rb
+++ lib/conta_luz.rb
@@ -3,7 +3,7 @@
 require 'data_nominal'
 
 class ContaLuz
- # codigo para implementar
+ # codigo implementado: Principais metodos que acessam e armazenam dados
     attr_accessor :formato_europeu
     attr_accessor :qtd_kw_gasto
     attr_accessor :valor_pagar
@@ -13,15 +13,17 @@ class ContaLuz
     attr_reader :vencimento
     attr_reader :emissao
 
+#metodo incializador
     def initialize
         @formato_europeu = '%d/%m/%Y'
     end
 
+#metodos que modificam dados
     def vencimento=(string)        
         @vencimento = Date.strptime(string, formato_europeu)
     end
 
-    def formatar_data(data_p)#adcionar um metodo attr_reader para vencimento e trocar metodo de impressão
+    def formatar_data(data_p)
         data_p.day.to_s << "/" << data_p.month.to_s << "/" << data_p.year.to_s
     end
 
--- spec/conta_luz_spec.rb
+++ spec/conta_luz_spec.rb
@@ -8,7 +8,7 @@ describe ContaLuz do
     subject(:conta_luz) { ContaLuz.new() }
 	let(:data_nominal) { double("nominal").as_null_object }
 
-
+ 
     describe "#qtdKwGasto" do
         it "devolve a quantidade de kw gasto" do
             qtd_kw_gasto = 460
@@ -28,169 +28,179 @@ describe ContaLuz do
         end        
     end# fim do describe #qtdKwGasto
 
+
     describe "#valorPagar" do
         it "armazena o valor a pagar" do
-            valor_pagar = 206,43
+          valor_pagar = 206,43
 
-            expect(conta_luz).to receive(:valor_pagar=).with(valor_pagar)
+          expect(conta_luz).to receive(:valor_pagar=).with(valor_pagar)
 
-            conta_luz.valor_pagar= 206,43
+          conta_luz.valor_pagar= 206,43
         end
 
         it "retorna o valor a ser pago" do
-            valor_pagar = 206,43
-            conta_luz.valor_pagar= valor_pagar
+          valor_pagar = 206,43
+          conta_luz.valor_pagar= valor_pagar
 
-            expect(conta_luz.valor_pagar).to eq(valor_pagar)
+          expect(conta_luz.valor_pagar).to eq(valor_pagar)
 
-            conta_luz.valor_pagar
+          conta_luz.valor_pagar
         end
 
     end#fim do describe #valorPagar
 
+
     describe "#numeroLeitura" do
         it "armazena numero de leitura" do
-            numero_leitura = 4166
+          numero_leitura = 4166
 
-            expect(conta_luz).to receive(:numero_leitura=).with(numero_leitura)
+          expect(conta_luz).to receive(:numero_leitura=).with(numero_leitura)
 
-            conta_luz.numero_leitura= 4166
+          conta_luz.numero_leitura= 4166
         end
 
         it "recupera numero de leitura" do
-            numero_leitura = 4166
-            conta_luz.numero_leitura= numero_leitura
+          numero_leitura = 4166
+          conta_luz.numero_leitura= numero_leitura
 
-            expect(conta_luz.numero_leitura).to eq(numero_leitura)
+          expect(conta_luz.numero_leitura).to eq(numero_leitura)
 
-            conta_luz.numero_leitura
+          conta_luz.numero_leitura
         end
     end# fim do describe numero_leitura
 
+
     describe "#vencimento" do
         it "armazena a data de vencimento" do
-            date = "15/07/2005"
+          date = "15/07/2005"
 
-            expect(conta_luz).to receive(:vencimento=).with(date)
+          expect(conta_luz).to receive(:vencimento=).with(date)
 
-            conta_luz.vencimento= "15/07/2005"
+          conta_luz.vencimento= "15/07/2005"
         end
 
         it "retorna a data de vencimento" do
-            date = Date.strptime("15/07/2005",'%d/%m/%Y')
-            conta_luz.vencimento= "15/07/2005"
+          date = Date.strptime("15/07/2005",'%d/%m/%Y')
+          conta_luz.vencimento= "15/07/2005"
 
-            expect(conta_luz.vencimento).to eq(date)
+          expect(conta_luz.vencimento).to eq(date)
 
-            conta_luz.vencimento
+          conta_luz.vencimento
         end
         
     end#fim do describe #vencimento
 
+
     describe "#formatar_data" do
         it "retorna o vencimento no formato string" do
-            date = "15/7/2005"
-            conta_luz.vencimento = "15/07/2005"
+          date = "15/7/2005"
+          conta_luz.vencimento = "15/07/2005"
 
-            expect(conta_luz.formatar_data(conta_luz.vencimento)).to eq(date)
+          expect(conta_luz.formatar_data(conta_luz.vencimento)).to eq(date)
 
-            conta_luz.formatar_data(conta_luz.vencimento)
+          conta_luz.formatar_data(conta_luz.vencimento)
         end
 
         it "retorna a data da emissão no formato string" do
-            date = "4/7/2005"
-            conta_luz.emissao = "04/07/2005"
+          date = "4/7/2005"
+          conta_luz.emissao = "04/07/2005"
 
-            expect(conta_luz.formatar_data(conta_luz.emissao)).to eq(date)
+          expect(conta_luz.formatar_data(conta_luz.emissao)).to eq(date)
 
-            conta_luz.formatar_data(conta_luz.emissao)
+          conta_luz.formatar_data(conta_luz.emissao)
         end
 
     end#fim do describe #formatar_data
 
+
     describe "#data_emissao" do
         it "armazena data de emissão" do
-            date = "04/07/2005"
+          date = "04/07/2005"
 
-            expect(conta_luz).to receive(:emissao=).with(date)
+          expect(conta_luz).to receive(:emissao=).with(date)
 
-            conta_luz.emissao= "04/07/2005"
+          conta_luz.emissao= "04/07/2005"
         end
 
         it "retorna data de emissao" do
-            date = Date.strptime("4/7/2005", '%d/%m/%Y')
-            conta_luz.emissao= "04/07/2005"
+          date = Date.strptime("4/7/2005", '%d/%m/%Y')
+          conta_luz.emissao= "04/07/2005"
 
-            expect(conta_luz.emissao).to eq(date)
+          expect(conta_luz.emissao).to eq(date)
 
-            conta_luz.emissao
+          conta_luz.emissao
         end
 
     end# fim do describe data_vencimento
 
+
     describe "#mes" do
         it "armazena o mes da conta" do
-            mes = 6
+          mes = 6
 
-           expect(conta_luz).to receive(:mes=).with(mes) 
+          expect(conta_luz).to receive(:mes=).with(mes) 
 
-            conta_luz.mes= mes         
+          conta_luz.mes= mes         
         end
 
         it "o valor do mes é um inteiro" do
-            mes = 6
-            conta_luz.mes= mes
+          mes = 6
+          conta_luz.mes= mes
 
-            expect(conta_luz.mes).to be_a_kind_of(Fixnum)
+          expect(conta_luz.mes).to be_a_kind_of(Fixnum)
 
-            conta_luz.mes
+          conta_luz.mes
         end
 
         it "retorna o mes da conta" do
-            mes = 6
-            conta_luz.mes= 6
+          mes = 6
+          conta_luz.mes= 6
 
-            expect(conta_luz.mes).to eq(mes)
+          expect(conta_luz.mes).to eq(mes)
 
-            conta_luz.mes
+          conta_luz.mes
         end
         
     end#fim do describe mes
 
+
     describe "#ano" do
         it "armazena o ano da conta" do
-            ano = 2005
+          ano = 2005
             
-            expect(conta_luz).to receive(:ano=).with(ano)
+          expect(conta_luz).to receive(:ano=).with(ano)
 
-            conta_luz.ano= 2005
+          conta_luz.ano= 2005
         end
         it "retorna o ano da conta" do
-            ano = 2005
-            conta_luz.ano= 2005
+          ano = 2005
+          conta_luz.ano= 2005
 
-            expect(conta_luz.ano).to eq(ano)
+          expect(conta_luz.ano).to eq(ano)
 
-            conta_luz.ano
+          conta_luz.ano
         end
     end#fim do describe ano
 
+
     describe "#nominal"  do
         it "retorna o mes nominal" do
-            conta_luz.mes= 6
+          conta_luz.mes= 6
 
-            expect(conta_luz.nominal).to eq("Julho")
+          expect(conta_luz.nominal).to eq("Julho")
 
-            conta_luz.nominal
+          conta_luz.nominal
         end
 
         it "armazena o mes nominal" do
-            conta_luz.mes= 6
+          conta_luz.mes= 6
 
-            expect(conta_luz).to receive(:nominal)
+          expect(conta_luz).to receive(:nominal)
 
-            conta_luz.nominal
+          conta_luz.nominal
         end
     end#fim do describe nominal
+
     
+
 end# fim do teste da classe ContaLuz
