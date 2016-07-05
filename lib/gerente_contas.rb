39f3f5e91ec940295beae0ebd0cb84b1534a3b16
jefferson abreu <nabucodonosor310@gmail.com>
2016-07-05 19:43:48 -0300

    primeira #entrada de dados falhou

--- lib/gerente_contas.rb
+++ lib/gerente_contas.rb
@@ -2,16 +2,19 @@
 # Gerencia o acesso as contas e seu armazenamento
 # versao 1.1 armazena somente contas de luz
 
+require 'conta_luz'
+
 class GerenteContas
 
 # inicializa o array de contas
   def initialize()
     listaContas = []
+    conta = ContaLuz.new
   end
 
 # principal entrada de dados das contas
-  def entrada_de_dados()
-    
+  def entrada_de_dados(qtd)
+    conta.qtd_kw_gasto = qtd
   end
 
 end
--- spec/gerente_conta_spec.rb
+++ spec/gerente_conta_spec.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 
-require "gerente_contas"
-require "spec_helper"
+require 'gerente_contas'
+require 'spec_helper'
 
 describe GerenteContas do
   # implementaremos testes dos métodos
@@ -10,7 +10,14 @@ describe GerenteContas do
   let(:conta_luz) { double("conta").as_null_object }
 
   describe "#entrada de dados" do
-    it "aramazena a quantidade de kw Gasto dado"
+    it "aramazena a quantidade de kw Gasto dado" do
+      allow(conta_luz).to receive(:qtd_kw_gasto).and_return(460)
+      qtd = 460
+
+      expect(conta_luz).to have_received(:qtd_kw_gasto).with(qtd)
+
+      gerente_contas.entrada_de_dados(qtd)
+    end
     it "armazena a quantidade de o valor a pagar"
     it "armazena o número de leitura da conta"
     it "armazena o mes"
