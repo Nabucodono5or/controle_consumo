f9935aa754c74362f656687700165ab3befde866
jefferson abreu <nabucodonosor310@gmail.com>
2016-08-09 22:07:23 -0300

    #gerente_contas para seis argumentos failed

--- spec/gerente_conta_spec.rb
+++ spec/gerente_conta_spec.rb
@@ -19,7 +19,11 @@ describe GerenteContas do
 
       gerente_contas.entrada_de_dados(460, 206.43, 4166, 6, 2005)
     end
-    it "aramazena a data de emissao como argumento"
+    it "aramazena a data de emissao como argumento" do
+      expect(gerente_contas).to respond_to(:entrada_de_dados).with(6).arguments
+
+      gerente_contas.entrada_de_dados(460, 206.43, 4166, 6, 2005, "4/7/2005")
+    end
     it "armazena a data de vencimento como argumento"
   end
 end
