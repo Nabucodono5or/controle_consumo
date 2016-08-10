407f33b3e851e2f7b3b96d7adeafe636386bc844
jefferson abreu <nabucodonosor310@gmail.com>
2016-08-09 22:38:27 -0300

    #entrada_de_dados sete argumentos refatorados

--- spec/gerente_conta_spec.rb
+++ spec/gerente_conta_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 
 describe GerenteContas do
   # implementaremos testes dos métodos
-  # testaremos se todos os argumentos foram enviados/recebidos
+  # testaremos se todos os argumentos foram enviados/recebidos (passed)
   # testaremos se o array da inicialização é incrementado
   # testaremos se possível a inicialização da classe ContaLuz
   # testaremos os métodos de comparação de contas
@@ -14,7 +14,7 @@ describe GerenteContas do
   let(:conta) { double("conta") }
 
   describe "#entrada de dados" do
-    it "armazena a data de vencimento como argumento" do
+    it "armazena sete argumentos lançados para serem instaciados em uma classe" do
       expect(gerente_contas).to respond_to(:entrada_de_dados).with(7).arguments
 
       gerente_contas.entrada_de_dados(460, 206.43, 4166, 6, 2005, "4/7/2005", "15/07/2005")
