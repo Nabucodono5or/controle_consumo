e8fa300dc130ed01e7cf72883f62b8e3819259f5
jefferson abreu <nabucodonosor310@gmail.com>
2016-08-10 21:43:12 -0300

    refatoramento dos testes e #mesMenorConsumo para fazer

--- spec/gerente_conta_spec.rb
+++ spec/gerente_conta_spec.rb
@@ -7,8 +7,8 @@ describe GerenteContas do
   # implementaremos testes dos métodos
   # testaremos se todos os argumentos foram enviados/recebidos (passed)
   # testaremos se inicializamos uma conta de luz (passed)
-  # testaremos se o array da inicialização é incrementado
-  # testaremos se possível a inicialização da classe ContaLuz
+  # testaremos se o array da inicialização é incrementado (passed)
+  # testaremos se possível a inicialização da classe ContaLuz (passed)
   # testaremos os métodos de comparação de contas
 
   subject(:gerente_contas) { GerenteContas.new() }
@@ -21,7 +21,7 @@ describe GerenteContas do
     end
 
     # a princípio a lista será privada
-    it "A lista de contas de estar vazia" do
+    it "A lista de contas deve estar vazia" do
       expect(gerente_contas.instance_variable_get(:@listaContas)).to be_empty
     end
   end
@@ -33,18 +33,24 @@ describe GerenteContas do
       gerente_contas.entrada_de_dados(460, 206.43, 4166, 6, 2005, "4/7/2005", "15/07/2005")
     end
 
-    it "O array será incrementado em um" do
+    it "O array pode ser incrementado em um" do
       gerente_contas.entrada_de_dados(460, 206.43, 4166, 6, 2005, "4/7/2005", "15/07/2005")
 
       expect(gerente_contas.instance_variable_get(:@listaContas)).not_to be_empty
     end
 
-    it "O array será incrementado em dois" do
+    it "O array pode ser incrementado em dois" do
       gerente_contas.entrada_de_dados(460, 206.43, 4166, 6, 2005, "4/7/2005", "15/07/2005")
       gerente_contas.entrada_de_dados(350, 157.07, 4201, 8, 2005, "02/08/2005", "15/08/2005")
       lista = gerente_contas.instance_variable_get(:@listaContas)
 
       expect(lista.length).to be > 1
     end
+  end
+
+  describe "#vereficarMesMenorConsumo" do
+    it "somente compara caso haja mais de duas contas adicionadas"
+    it "compara o qtdKwGasto entre todas as contas na lista de contas"
+    it "retorna uma variavel com o resultado da conta de menor consumo"
   end
 end
