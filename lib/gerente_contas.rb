6707a4f1e6ce9ebccbb0af6dc8f246c745ad68c3
jefferson abreu <nabucodonosor310@gmail.com>
2016-08-17 18:35:28 -0300

    gerente_contas remodelado

--- lib/gerente_contas.rb
+++ lib/gerente_contas.rb
@@ -26,46 +26,34 @@ class GerenteContas
   end
 
   def mes_menor_consumo
-    calculo_mes_menor unless testando_lista_de_contas
+    if calculo
+      puts "Mes #{@menor.nominal}, consumo de #{@menor.qtd_kw_gasto} kw/h"
+    end
   end # end method mes_menor_consumo
 
   def mes_maior_consumo
-    calculo_mes_maior unless testando_lista_de_contas
+    if calculo
+      puts "Mes #{@maior.nominal}, consumo de #{@maior.qtd_kw_gasto} kw/h"
+    end
   end
 
   private
 
-  # calculo_mes_menor e  calculo_mes_maior ambos repetem a mesma linha de calculo
-  # isso pode ser melhorado com a criação de um novo método privado que trata o calculo de
-  # maior ou menor consumo diferenciando apenas nas variáveis/ porém isso trata de
-  # ter acumular vairáveis de instacia
-
   def calculo
-    @menor ||= @maior ||= @lista_contas[0]
+    if @lista_contas != []
+      @menor ||= @maior ||= @lista_contas[0]
 
-    @lista_contas.each do |conta|
-      if conta.qtd_kw_gasto < @menor.qtd_kw_gasto
-        @menor = conta
+      @lista_contas.each do |conta|
+        if conta.qtd_kw_gasto < @menor.qtd_kw_gasto
+          @menor = conta
+        end
+
+        if conta.qtd_kw_gasto > @maior.qtd_kw_gasto
+          @maior = conta
+        end
       end
 
-      if conta.qtd_kw_gasto > @maior.qtd_kw_gasto
-        @maior = conta
-      end
-    end
-  end
-
-  def calculo_mes_menor
-    calculo
-    puts "Mes #{@menor.nominal}, consumo de #{@menor.qtd_kw_gasto} kw/h"
-  end
-
-  def calculo_mes_maior
-    calculo
-    puts "Mes #{@maior.nominal}, consumo de #{@maior.qtd_kw_gasto} kw/h"
-  end
-
-  def testando_lista_de_contas
-    if @lista_contas == []
+    else
       raise RuntimeError, "Nenhuma conta cadastrada"
     end
   end
