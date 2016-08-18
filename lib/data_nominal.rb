37192c68ed38f39af9eef692f9710f1c83816f70
jefferson abreu <nabucodonosor310@gmail.com>
2016-08-17 21:17:56 -0300

    refatorado DataNominal

--- lib/data_nominal.rb
+++ lib/data_nominal.rb
@@ -1,21 +1,22 @@
 # encoding: UTF-8
 
-#Classe que converte um mes numérico em sua representação nominal
-
+# Classe que converte um mes numérico em sua representação nominal
 class DataNominal
-    
-    def initialize(mes)
-        @mes = mes
-        @mes += 1    
-    end
+  def initialize(mes)
+    @mes = mes
+    @mes += 1
+  end
 
-    def nominal
-        @nome_mes = converte
-    end
+  def nominal
+    @nome_mes = converte
+  end
 
-    def converte
-      array_mes = ["Janeiro", "Fevereiro", "Março", "Maio", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
-      array_mes[@mes]
-    end
-    
-end#fim da classe DataNominal
+  def converte
+    array_mes =
+      %W(
+        Janeiro Fevereiro Março Maio Abril Maio Junho Julho Agosto
+        Setembro Outubro Novembro Dezembro
+      )
+    array_mes[@mes]
+  end
+end # fim da classe DataNominal
