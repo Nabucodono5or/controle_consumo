15cda526460c21d0474ad7519eba1747d6e2d9fc
Jefferson Abreu <nabucodonosor310@gmail.com>
2016-08-18 09:19:16 -0300

    Cli_Ui e Client criados e teste #menu failed

---
+++ lib/client.rb
@@ -0,0 +1,7 @@
+# encoding: UTF-8
+
+require 'cli_ui'
+
+class Client
+  #TODO implementation
+end
---
+++ spec/client_spec.rb
@@ -0,0 +1,13 @@
+# encoding: UTF-8
+
+require 'client'
+require 'spec_helper'
+
+describe Client do
+  subject(:client) { Client.new }
+
+  describe '#menu' do
+    it 'exibe um menu de informação'
+  end
+
+end
