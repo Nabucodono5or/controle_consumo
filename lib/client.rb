3b6bcfc1f5febc99400336d5aafd14ec81607b8f
Jefferson Abreu <nabucodonosor310@gmail.com>
2016-08-18 09:30:08 -0300

    #menu teste de saida passed

--- lib/client.rb
+++ lib/client.rb
@@ -3,5 +3,13 @@
 require 'cli_ui'
 
 class Client
-  #TODO implementation
+  def initialize
+    @cli_ui = CLiUi.new
+  end
+
+  def menu
+    texto = 'será exibido um menu aqui'
+
+    @cli_ui.write(texto)
+  end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -7,7 +7,11 @@ describe Client do
   subject(:client) { Client.new }
 
   describe '#menu' do
-    it 'exibe um menu de informação'
+    it 'exibe um menu de informação' do
+      texto = "será exibido um menu aqui\n"
+
+      expect{ client.menu }.to output(texto).to_stdout
+    end
   end
 
 end
